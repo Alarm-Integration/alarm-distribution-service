e7cdbeeede1504bcfff6555db45308bd23226975
Root(김명석) <mskim@gabia.com>
2021-06-14 10:40:44 +0900

    test: 공통 Response 값 구현 및 테스트

---
+++ src/main/java/com/gabia/alarmdistribution/vo/ResponseObject.java
@@ -0,0 +1,12 @@
+package com.gabia.alarmdistribution.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseObject {
+    private String message;
+
+    private Object result;
+}
