2c24fc0b5922a5a5e08e7c5b12caac01406da1f3
Root(김명석) <mskim@gabia.com>
2021-06-14 10:22:03 +0900

    test: AlarmDistributionServiceApplication 테스트

--- src/test/java/com/gabia/alarmdistribution/AlarmDistributionServiceApplicationTests.java
+++ src/test/java/com/gabia/alarmdistribution/AlarmDistributionServiceApplicationTests.java
@@ -1,13 +1,12 @@
 package com.gabia.alarmdistribution;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AlarmDistributionServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        AlarmDistributionServiceApplication.main(new String[]{});
     }
 
 }
