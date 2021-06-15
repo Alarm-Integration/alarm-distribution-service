e1b4a75e79c8441f6bcd0724080e558dc656e52e
Root(김명석) <mskim@gabia.com>
2021-06-15 10:36:55 +0900

    test: kafka sms 테스트

--- src/main/java/com/gabia/alarmdistribution/service/SMSService.java
+++ src/main/java/com/gabia/alarmdistribution/service/SMSService.java
@@ -1,5 +1,7 @@
 package com.gabia.alarmdistribution.service;
 
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -7,8 +9,25 @@ import java.util.Map;
 @Service
 public class SMSService implements SendService {
 
+    private Environment env;
+    private KafkaTemplate<String, Map<String, Object>> kafkaTemplate;
+
+    public SMSService(Environment env, KafkaTemplate<String, Map<String, Object>> kafkaTemplate) {
+        this.env = env;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public String send(Map<String, Object> map) {
-        return null;
+        String senderNumber = getSenderNumber(1L);
+        map.put("sender", senderNumber);
+        kafkaTemplate.send(env.getProperty("topic.sms"), map);
+
+        return "성공";
+    }
+
+    // 사용자 userId 값으로 보내는 번호(sender) 받아오기
+    public String getSenderNumber(Long userId) {
+        return "1544-4370";
     }
 }
---
+++ src/test/java/com/gabia/alarmdistribution/service/SMSServiceTest.java
@@ -0,0 +1,100 @@
+package com.gabia.alarmdistribution.service;
+
+import com.gabia.alarmdistribution.vo.request.Raw;
+import com.gabia.alarmdistribution.vo.request.RequestAlarmCommon;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class SMSServiceTest {
+
+    @Mock
+    private KafkaTemplate<String, Map<String, Object>> kafkaTemplate;
+
+    @Mock
+    private Environment env;
+
+    @InjectMocks
+    private SMSService smsService;
+
+    @Test
+    void getSenderNumberTest(){
+        // Inmemory DB 삽입(userId, number);
+
+        // DB 접속 및 senderNumber 가져오기
+        // String senderNumber =  getSenderNumber(userId);
+        String senderNumber = smsService.getSenderNumber(1L);
+
+        assertThat(senderNumber).isEqualTo("");
+    }
+
+    @Test
+    void sendTest(){
+        // given
+        String appName = "sms";
+
+        ArrayList<String> address = new ArrayList<>() {
+            {
+                add("01012341234");
+                add("01043214321");
+            }
+        };
+
+        Raw raw = new Raw();
+        raw.setAppName(appName);
+        raw.setAddress(address);
+        ArrayList<Raw> raws = new ArrayList<>(){
+            {
+                add(raw);
+            }
+        };
+
+
+        Long groupId = 1L;
+        String title = "알림 제목";
+        String content = "알림 내용";
+        ArrayList<Integer> bookmarks = new ArrayList<>(){
+            {
+                add(1);
+            }
+        };
+
+        RequestAlarmCommon alarmCommon = new RequestAlarmCommon();
+        alarmCommon.setGroupId(groupId);
+        alarmCommon.setTitle(title);
+        alarmCommon.setContent(content);
+        alarmCommon.setBookmarks(bookmarks);
+        alarmCommon.setRaws(raws);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("groupId", alarmCommon.getGroupId());
+        map.put("title", alarmCommon.getTitle());
+        map.put("content", alarmCommon.getTitle());
+        map.put("bookmarks", alarmCommon.getBookmarks());
+        map.put("raws", raws);
+
+        ListenableFuture listenableFuture = mock(ListenableFuture.class);
+        given(env.getProperty("topic.sms")).willReturn("sms");
+        given(kafkaTemplate.send(env.getProperty("topic.sms"), map)).willReturn(listenableFuture);
+
+        // when
+        String send = smsService.send(map);
+
+        // then
+        assertThat(send).isEqualTo("성공");
+    }
+}
