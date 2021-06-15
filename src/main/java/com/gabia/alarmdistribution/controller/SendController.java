639283674c9726add9ba81a8850c61500c221bdd
Root(김명석) <mskim@gabia.com>
2021-06-15 13:30:41 +0900

    fix: sendService 생성자 주입
    + Eureka service client 등록

--- src/main/java/com/gabia/alarmdistribution/controller/SendController.java
+++ src/main/java/com/gabia/alarmdistribution/controller/SendController.java
@@ -1,5 +1,6 @@
 package com.gabia.alarmdistribution.controller;
 
+import com.gabia.alarmdistribution.service.SendServiceImpl;
 import com.gabia.alarmdistribution.vo.ResponseObject;
 import com.gabia.alarmdistribution.vo.request.RequestAlarmCommon;
 import org.springframework.http.HttpStatus;
@@ -11,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SendController {
 
+    private SendServiceImpl sendService;
+
+    public SendController(SendServiceImpl sendService) {
+        this.sendService = sendService;
+    }
+
     @PostMapping
     public ResponseEntity<ResponseObject> sendAlarm(@RequestBody RequestAlarmCommon request){
         ResponseObject response = new ResponseObject();
         response.setMessage("알림 전송 요청 완료");
+        response.setResult(sendService.send(request));
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
