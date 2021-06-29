9b2462a1671f08c52a1ac04fc2d96ff9fb8335ad
Dexter(노영삼) <nys@gabia.com>
2021-06-29 15:22:16 +0900

    modify: SMSService 작성 - 레코드가 브로커에 적재되는지 확인하기 위한 콜백 추가 - SMSService send 메서드 반환 값 변경

--- src/main/java/com/gabia/alarmdistribution/service/SMSService.java
+++ src/main/java/com/gabia/alarmdistribution/service/SMSService.java
@@ -1,26 +1,49 @@
 package com.gabia.alarmdistribution.service;
 
-import org.springframework.core.env.Environment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.Map;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service("sms")
 public class SMSService implements SendService {
 
-    private Environment env;
-    private KafkaTemplate<String, Map<String, Object>> kafkaTemplate;
-
-    public SMSService(Environment env, KafkaTemplate<String, Map<String, Object>> kafkaTemplate) {
-        this.env = env;
-        this.kafkaTemplate = kafkaTemplate;
-    }
+    private final KafkaTemplate<String, Map<String, Object>> kafkaTemplate;
 
     @Override
-    public boolean send(Map<String, Object> data) {
-        kafkaTemplate.send(env.getProperty("topic.sms"), data);
+    public ListenableFuture<SendResult<String, Map<String, Object>>> send(Map<String, Object> data) {
+        ListenableFuture<SendResult<String, Map<String, Object>>> future = kafkaTemplate.send("sms", data);
 
-        return true;
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(data, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Map<String, Object>> result) {
+                handleSuccess(data);
+            }
+        });
+        return future;
+    }
+
+    private void handleSuccess(Map<String, Object> data) {
+        String traceId = (String) data.get("traceId");
+        Long userId = (Long) data.get("userId");
+        log.info("{}: userId:{} traceId:{} massage:{}", getClass().getSimpleName(), userId, traceId, "메세지 적재 성공");
+    }
+
+    private void handleFailure(Map<String, Object> data, Throwable ex) {
+        Long userId = (Long) data.get("userId");
+        String traceId = (String) data.get("traceId");
+        log.error("{}: userId:{} traceId:{} massage:{}", getClass().getSimpleName(), userId, traceId, ex.getMessage());
     }
 }
