6bde5a5031a8e27b6056f39d4ec55c498be56244
Dexter(노영삼) <nys@gabia.com>
2021-06-29 13:05:06 +0900

    test: EmailServiceTest - send_메서드_레코드_적재_성공 작성 - send_메서드_레코드_적재_실패 작성

--- src/test/java/com/gabia/alarmdistribution/service/EmailServiceTest.java
+++ src/test/java/com/gabia/alarmdistribution/service/EmailServiceTest.java
@@ -1,21 +1,32 @@
 package com.gabia.alarmdistribution.service;
 
-import com.gabia.alarmdistribution.dto.request.Raw;
-import com.gabia.alarmdistribution.dto.request.CommonAlarmRequest;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import com.gabia.alarmdistribution.util.MemoryAppender;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.env.Environment;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.SettableListenableFuture;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 class EmailServiceTest {
@@ -23,52 +34,90 @@ class EmailServiceTest {
     @Mock
     private KafkaTemplate<String, Map<String, Object>> kafkaTemplate;
 
-    @Mock
-    private Environment env;
-
     @InjectMocks
     private EmailService emailService;
 
+    private MemoryAppender memoryAppender;
+
+    //default
+    private Long groupId = 1L;
+    private Long userId = 1L;
+    private String sender = "sender@email.com";
+    private List<String> raws = Arrays.asList("test@gmail.com", "t123@gmail.com", "test@naver.com");
+    private String title = "알림 제목";
+    private String content = "알림 내용";
+    private String traceId = "abc";
+
+    @BeforeEach
+    public void setup() {
+        Logger logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        memoryAppender = new MemoryAppender();
+        memoryAppender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
+        logger.setLevel(Level.INFO);
+        logger.addAppender(memoryAppender);
+        memoryAppender.start();
+    }
+
     @Test
-    void sendTest() {
+    void send_메서드_레코드_적재_성공() throws ExecutionException, InterruptedException {
         // given
-        Long groupId = 1L;
-        String title = "알림 제목";
-        String content = "알림 내용";
+        Map<String, Object> data = createDefaultData();
 
-        String appName = "email";
-        List<String> address = Arrays.asList("test@gmail.com", "t123@gmail.com", "test@naver.com");
-        Raw raw = Raw.builder()
-                .appName(appName)
-                .address(address)
-                .build();
-        List<Raw> raws = Arrays.asList(raw);
+        ProducerRecord<String, Map<String, Object>> record = new ProducerRecord<>("email", data);
 
-        List<Integer> bookmarksIds = Arrays.asList(1);
+        RecordMetadata recordMetadata = new RecordMetadata(new TopicPartition("email", 1),
+                1,1,342, System.currentTimeMillis(), 1, 2);
 
-        CommonAlarmRequest commonAlarmRequest = CommonAlarmRequest.builder()
-                .groupId(groupId)
-                .title(title)
-                .content(content)
-                .bookmarks(bookmarksIds)
-                .raws(raws)
-                .build();
+        SendResult<String, Map<String, Object>> sendResult= new SendResult<>(record, recordMetadata);
 
-        Map<String, Object> map = new HashMap<>();
-        map.put("groupId", commonAlarmRequest.getGroupId());
-        map.put("title", commonAlarmRequest.getTitle());
-        map.put("content", commonAlarmRequest.getTitle());
-        map.put("bookmarks", commonAlarmRequest.getBookmarks());
-        map.put("raws", raws);
+        SettableListenableFuture future = new SettableListenableFuture<>();
+        future.set(sendResult);
 
-        ListenableFuture listenableFuture = mock(ListenableFuture.class);
-        given(env.getProperty("topic.email")).willReturn("email");
-        given(kafkaTemplate.send(env.getProperty("topic.email"), map)).willReturn(listenableFuture);
+        given(kafkaTemplate.send("email", data)).willReturn(future);
 
         // when
-        boolean isSend = emailService.send(map);
+        ListenableFuture<SendResult<String, Map<String, Object>>> listenableFuture = emailService.send(data);
 
-        // then
-        assertThat(isSend).isEqualTo(true);
+        //then
+        SendResult<String, Map<String, Object>> result = listenableFuture.get();
+        assertThat(result.getProducerRecord().value().get("groupId")).isEqualTo(groupId);
+        assertThat(result.getProducerRecord().value().get("userId")).isEqualTo(userId);
+        assertThat(result.getProducerRecord().value().get("sender")).isEqualTo(sender);
+        assertThat(result.getProducerRecord().value().get("raws")).isEqualTo(raws);
+        assertThat(result.getProducerRecord().value().get("title")).isEqualTo(title);
+        assertThat(result.getProducerRecord().value().get("content")).isEqualTo(content);
+        assertThat(result.getProducerRecord().value().get("traceId")).isEqualTo(traceId);
+        assertThat(memoryAppender.getSize()).isEqualTo(1);
+        assertThat(memoryAppender.contains(String.format("EmailService: userId:%s traceId:%s 메세지 적재 성공", userId, traceId), Level.INFO)).isTrue();
+    }
+
+    @Test
+    void send_메서드_레코드_적재_실패() {
+        // given
+        Map<String, Object> data = createDefaultData();
+
+        SettableListenableFuture future = new SettableListenableFuture<>();
+        future.setException(new RuntimeException("Exception Calling Kafka"));
+
+        given(kafkaTemplate.send("email", data)).willReturn(future);
+
+        // when
+        emailService.send(data);
+
+        //then
+        assertThat(memoryAppender.getSize()).isEqualTo(1);
+        assertThat(memoryAppender.contains(String.format("EmailService: userId:%s traceId:%s 메세지 적재 실패 %s", userId, traceId, "Exception Calling Kafka"), Level.ERROR)).isTrue();
+    }
+
+    private Map<String, Object> createDefaultData() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("groupId", groupId);
+        data.put("userId", userId);
+        data.put("sender", sender);
+        data.put("raws", raws);
+        data.put("title", title);
+        data.put("content", content);
+        data.put("traceId", traceId);
+        return data;
     }
 }
