65770f20e2b7f0aa43d0c416c0d33b988cf0eb60
Dexter(노영삼) <nys@gabia.com>
2021-07-08 15:00:24 +0900

    modify: 임시

--- src/main/java/com/gabia/alarmdistribution/dto/response/APIResponse.java
+++ src/main/java/com/gabia/alarmdistribution/dto/response/APIResponse.java
@@ -8,13 +8,6 @@ public class APIResponse {
     private String message;
     private Object result;
 
-    public static APIResponse withMessage(String message) {
-        return APIResponse.builder()
-                .message(message)
-                .result(null)
-                .build();
-    }
-
     public static APIResponse withMessageAndResult(String message, Object result) {
         return APIResponse.builder()
                 .message(message)
