2cf615c3a52e11601f284dfab5a945f9947d1c88
Dexter(노영삼) <nys@gabia.com>
2021-07-17 20:15:21 +0900

    feat: 통합 로그 기능

--- src/test/java/com/gabia/alarmdistribution/service/AlarmServiceTest.java
+++ src/test/java/com/gabia/alarmdistribution/service/AlarmServiceTest.java
@@ -4,6 +4,7 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import com.gabia.alarmdistribution.dto.request.AlarmMessage;
+import com.gabia.alarmdistribution.util.LogSender;
 import com.gabia.alarmdistribution.util.MemoryAppender;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -27,6 +28,9 @@ import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class AlarmServiceTest {
+
+    @Mock
+    private LogSender logSender;
 
     @Mock
     private KafkaTemplate<String, AlarmMessage> kafkaTemplate;
